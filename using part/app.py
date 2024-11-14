cdee8f06d6264bc550d3acabddd93cafe5160714
dengruihan <551310554@qq.com>
2024-11-14 13:12:59 +0800

    乱扣费bug解决

--- using part/app.py
+++ using part/app.py
@@ -6,6 +6,7 @@ app = Flask(__name__)
 
 # 创建ZhipuAI实例，并传入APIKey
 client = ZhipuAI(api_key="5947c467381fbebbdb52372af7779960.BfqMTtsJAXW9neJi") # 请填写您自己的APIKey
+clientc = ZhipuAI(api_key="fed76c71e516c486e1bcc058fc6bf4ca.Lni1nsQevsfJvVhG")
 
 # 定义根路由，返回index.html页面
 @app.route('/')
@@ -21,7 +22,7 @@ def chat():
     response = client.chat.completions.create(
         model="glm-4-flash",  # 填写需要调用的模型名称
         messages=[
-            {"role": "system", "content": "你是一台先进的AI勋章设计助手，专长于根据用户的个人经历和成就，创造性地设计勋章。你的任务是倾听用户的经历描述，分析其中的关键信息。最后从画面元素/画面底色 三个角度输出设计方案。你需要在你的设计方案前加上‘徽章设计方案1’字样"},
+            {"role": "system", "content": "你是一台先进的AI勋章设计助手，专长于根据用户的个人经历和成就，创造性地设计勋章。你的任务是倾听用户的经历描述，分析其中的关键信息。最后从画面元素/画面底色 三个角度输出设计方案。你需要在你的设计方案前加上‘฿’字样"},
             {"role": "user","content": user_input}
         ],
     )
@@ -30,9 +31,9 @@ def chat():
     ai_response = response.choices[0].message.content
 
     #检索“徽章设计方案”字样
-    if "1" in ai_response:
+    if "฿" in ai_response:
         #把设计方案转交至cogview
-        responsec = client.images.generations(
+        responsec = clientc.images.generations(
         model="cogView-3-plus", #填写需要调用的模型编码
         prompt=ai_response,
         size="1024x1024"
